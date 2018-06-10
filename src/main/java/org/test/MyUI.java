4d92c620a754c335f2a5f5a1d1fa2cadab879050
Karl Grosse <grosse.karl@gmail.com>
2018-06-10 11:33:54 +0200

    added Country class which was excluded before due to .gitignore rules

--- src/main/java/org/test/MyUI.java
+++ src/main/java/org/test/MyUI.java
@@ -1,7 +1,5 @@
 package org.test;
 
-import javax.servlet.annotation.WebServlet;
-
 import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.VaadinServletConfiguration;
 import com.vaadin.annotations.Widgetset;
@@ -18,21 +16,23 @@ import com.vaadin.ui.TextField;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
 import com.vaadin.v7.data.util.BeanItemContainer;
-import org.test.data.Country;
+import org.test.model.Country;
 
+import javax.servlet.annotation.WebServlet;
 import java.util.Arrays;
 
 /**
- * This UI is the application entry point. A UI may either represent a browser window 
+ * This UI is the application entry point. A UI may either represent a browser window
  * (or tab) or some part of an HTML page where a Vaadin application is embedded.
  * <p>
- * The UI is initialized using {@link #init(VaadinRequest)}. This method is intended to be 
+ * The UI is initialized using {@link #init(VaadinRequest)}. This method is intended to be
  * overridden to add component to the user interface and initialize non-component functionality.
  */
 @Theme("mytheme")
 @Widgetset("com.vaadin.v7.Vaadin7WidgetSet")
 public class MyUI extends UI {
 
+    private static final long serialVersionUID = 7860795845530359600L;
     private Panel panel;
 
     @Override
@@ -40,8 +40,10 @@ public class MyUI extends UI {
         VerticalLayout mainLayout = new VerticalLayout();
         VerticalLayout controlElements = new VerticalLayout();
         TextField tf = new TextField("Enter number of Comboboxes");
+        tf.setValue("1");
         RadioButtonGroup rbg = new RadioButtonGroup<String>("Vaadin version");
         rbg.setItems("Vaadin 7", "Vaadin 8");
+        rbg.setSelectedItem("Vaadin 7");
 
         Button btn = new Button("Create");
 
@@ -67,13 +69,11 @@ public class MyUI extends UI {
 
     private Component createContent(String vaadinType, Integer count) {
         VerticalLayout content = new VerticalLayout();
-        if ("Vaadin 7".equals(vaadinType)) {
-            for (int i = 0; i < count; i++) {
+        for (int i = 0; i < count; i++) {
+            if ("Vaadin 7".equals(vaadinType)) {
                 content.addComponent(createVaadin7Components());
             }
-        }
-        else {
-            for (int i = 0; i < count; i++) {
+            else {
                 content.addComponent(createVaadin8Components());
             }
         }
@@ -105,5 +105,6 @@ public class MyUI extends UI {
     @WebServlet(urlPatterns = "/*", name = "MyUIServlet", asyncSupported = true)
     @VaadinServletConfiguration(ui = MyUI.class, productionMode = false)
     public static class MyUIServlet extends VaadinServlet {
+        private static final long serialVersionUID = 5136219665371893640L;
     }
 }
